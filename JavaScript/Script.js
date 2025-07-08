abfbcf192c2474655af4bf6ac44f226515afaed8
jagan <jaganmohanreddykothapu780@gmial.com>
2025-07-08 20:42:03 +0530

    colours

---
+++ JavaScript/Script.js
@@ -0,0 +1,40 @@
+// console.log("javascript");
+
+// console.log("%c javascript", "color:blue")
+
+// console.log ("%c Javascript", "background-color:red")
+
+// console.log("%cjavascript program", "background-color:red")
+ 
+
+ function redcolor(){
+     console.log("%c    ", "background-color:red");
+ }
+
+ //redcolor();
+
+
+
+ function fristsection(){
+    console.log("%c    ", "background-color:purple");
+    console.log("%c    ", "background-color:lightgray");
+    redcolor();
+
+ }
+ fristsection();
+
+ function secondsection(){
+     console.log("%c    ", "background-color:orange");
+     console.log("%c    ", "background-color:black");
+     redcolor();
+
+ }
+ secondsection();
+
+ function thirdsection(){
+     console.log("%c    ", "background-color:green");
+     console.log("%c    ", "background-color:darkgrey");
+     redcolor();
+
+ }
+ thirdsection();
