c847ae3019fd5db6f0ad5fb04aa87774f2da6765
jagan <jaganmohanreddykothapu780@gmial.com>
2025-07-11 15:55:33 +0530

    goodstart

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,36 +1,8 @@
-//Student Result
-
-let Name=prompt("Enter your Name");
-
-let Marks=prompt("enter your marks");
-
-
-switch (true) {
-    case Marks >90 && Marks <=100:
-        result="Great you are the topper"
-    break;
-
-    case Marks >65 && Marks <=90:
-        result="you got frist rank"
-    break;
-
-    case Marks >=40 && Marks < 65:
-        result= "you got second rank"
-    break;
-
-    case Marks >=30 && Marks <= 40:
-        result="you got third rank"
-    break;
-
-    case Marks <30:
-        result= " sorry you got falied better luck next time"
-    break;
-
-    default:
-        result="please enter your marks"
-        break;
+function goodStart(){
+    console.log( "code test")
 }
 
-        document.write("Hello"+ Name + "<h2>"+ result+ "</h2>")
-
-
+goodStart();
+goodStart();
+goodStart();
+goodStart();
