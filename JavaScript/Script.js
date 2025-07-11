8a1206b0937b904f074a2e1c73b55c1fdf67f029
jagan <jaganmohanreddykothapu780@gmial.com>
2025-07-11 16:35:23 +0530

    global

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,8 +1,16 @@
-function goodStart(){
-    console.log( "code test")
+// Global scope
+// Function scope
+// Block Scope
+
+
+var Apple = "red";
+let orange = "yellow";
+const banana = "green";
+
+function fruit(){
+    document.write(Apple)
+    document.write(orange)
+    document.write(Apple)
 }
 
-goodStart();
-goodStart();
-goodStart();
-goodStart();
+fruit();
