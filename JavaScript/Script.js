b5ad48c735f81235badb344e638c9176b53bf193
jagan <jaganmohanreddykothapu780@gmial.com>
2025-07-14 06:59:41 +0530

    Function Expression

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,39 +1,10 @@
-// Global scope
-// Function scope
-// Block Scope
-
-function sports(){
-
-    if (10 < 20){
-
-        var tennis = "sania"
-        let cricket ="virat"
-        const ground = "stadium"
-       // document.write(cricket)
-        document.write(ground+ "<br>")
+// Function Expression
+// Anonymous Function
+// Function with parameters and Return value
 
 
-    }
-
-
-    document.write(tennis)
-
-
+var a = function(){
+    document.write("This is called function Expression")
 }
 
-
-
-sports()
-
-
-
-
-
-
-
-
-
-    
-
-
-
+a();
